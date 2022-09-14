576131f82830cdfc5c7f860e35ac306d7890ebe9
Rajat Jakhar <rajat_2001ce48@iitp.ac.in>
2022-09-14 14:25:41 +0530

    average u, v, w

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,8 +1,37 @@
-def factorial(x):
-    if(x ==1 or x==0):
-        return 1
-    return x*factorial(x-1)
-    
+# def octact_identification(mod=5000):
+# ###Code
 
-x=int(input("Enter the number whose factorial is to be found: "))
-print(factorial(x))
+
+# from platform import python_version
+# ver = python_version()
+
+# if ver == "3.8.10":
+#     print("Correct Version Installed")
+# else:
+#     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+# mod=5000
+# octact_identification(mod)
+# import pandas as pd
+# df = pd.read_csv (r'octant_output.csv')
+# print("sum of octant is ", df['Octant'].sum());
+import csv
+
+with open('octant_input.csv', 'r') as file:
+    sum_u =0
+    sum_v=0
+    sum_w =0
+    length =0
+    read = csv.reader(file)
+    for row in read:
+        length+=1
+        if(length == 1):
+            continue
+        sum_u = sum_u+float(row[1])
+        sum_v = sum_v +float(row[2])
+        sum_w = sum_w +float(row[3])
+    print(sum_u/length)
+    print(sum_v/length)
+    print(sum_w/length)
+
+        
