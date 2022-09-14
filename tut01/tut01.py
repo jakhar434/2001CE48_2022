9e9fb07b09f198a6dc63c32a737d9a4b2d4271b9
Rajat Jakhar <rajat_2001ce48@iitp.ac.in>
2022-09-14 19:27:36 +0530

    octant column added

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,37 +1,63 @@
-# def octact_identification(mod=5000):
-# ###Code
-
-
-# from platform import python_version
-# ver = python_version()
-
-# if ver == "3.8.10":
-#     print("Correct Version Installed")
-# else:
-#     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-# mod=5000
-# octact_identification(mod)
 # import pandas as pd
 # df = pd.read_csv (r'octant_output.csv')
 # print("sum of octant is ", df['Octant'].sum());
 import csv
+import pandas as pd
+file = pd.read_csv("octant_input.csv")
+#finding the average value of U, V AND W
+average_u = file['U'].mean()
+average_v = file['V'].mean()
+average_w = file['W'].mean()
+# adding this to csv file
+file.at[0, "U Avg"] = average_u
+file.at[0, "V Avg"] = average_v
+file.at[0, "W Avg"] = average_w
+# subtarcting the value from average values and adding to csv file
+file["U'=U - U avg"] = file['U']-average_u
+file["V'=V - V avg"] = file['V']-average_v
+file["W'=W - W avg"] = file['W']-average_w
 
-with open('octant_input.csv', 'r') as file:
-    sum_u =0
-    sum_v=0
-    sum_w =0
-    length =0
-    read = csv.reader(file)
-    for row in read:
-        length+=1
-        if(length == 1):
-            continue
-        sum_u = sum_u+float(row[1])
-        sum_v = sum_v +float(row[2])
-        sum_w = sum_w +float(row[3])
-    print(sum_u/length)
-    print(sum_v/length)
-    print(sum_w/length)
+# +ve, +ve 1st quadrant
+# -ve , +ve 2nd quadrant
+# -ve, -ve 3rd quadrant
+# +ve, -ve 4th quadrant
+octant_list =[]
+for x in range(len(file)):
+    
+    if (file["U'=U - U avg"][x] > 0 and file["V'=V - V avg"][x] > 0):
+        if (file["W'=W - W avg"][x] >= 0):
+            octant_list.append(1)
+        else:
+            octant_list.append(-1)
 
-        
+    elif (file["U'=U - U avg"][x] < 0 and file["V'=V - V avg"][x]> 0):
+        if (file["W'=W - W avg"][x] >= 0):
+            octant_list.append(2)
+        else:
+            octant_list.append(-2)
+
+    elif (file["U'=U - U avg"][x] < 0 and file["V'=V - V avg"][x] < 0):
+        if (file["W'=W - W avg"][x] >= 0):
+            octant_list.append(3)
+        else:
+            octant_list.append(-3)
+
+    else:
+        if (file["W'=W - W avg"][x] >= 0):
+            octant_list.append(4)
+        else:
+            octant_list.append(-4)
+
+file["octant"]= octant_list
+file.at[2, ' ']="User Input"
+file.at[0, 'Octant ID'] =' '
+file.at[0, '1'] = file["octant"].value_counts()[1]
+file.at[0, '-1'] = file["octant"].value_counts()[-1]
+file.at[0, '2'] = file["octant"].value_counts()[2]
+file.at[0, '-2'] = file["octant"].value_counts()[-2]
+file.at[0, '3'] = file["octant"].value_counts()[3]
+file.at[0, '-3'] = file["octant"].value_counts()[-3]
+file.at[0, '4'] = file["octant"].value_counts()[4]
+file.at[0, '-4'] = file["octant"].value_counts()[-4]
+
+file.to_csv("octant_1.csv")
