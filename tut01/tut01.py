bcb512022ad877228f8415a507105d9254e60608
Rajat Jakhar <rajat_2001ce48@iitp.ac.in>
2022-09-14 22:01:41 +0530

    octant id committed

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -2,17 +2,20 @@
 # df = pd.read_csv (r'octant_output.csv')
 # print("sum of octant is ", df['Octant'].sum());
 import csv
+import math
 import pandas as pd
 file = pd.read_csv("octant_input.csv")
-#finding the average value of U, V AND W
+# finding the average value of U, V AND W
 average_u = file['U'].mean()
 average_v = file['V'].mean()
 average_w = file['W'].mean()
 # adding this to csv file
+
 file.at[0, "U Avg"] = average_u
 file.at[0, "V Avg"] = average_v
 file.at[0, "W Avg"] = average_w
 # subtarcting the value from average values and adding to csv file
+
 file["U'=U - U avg"] = file['U']-average_u
 file["V'=V - V avg"] = file['V']-average_v
 file["W'=W - W avg"] = file['W']-average_w
@@ -21,16 +24,16 @@ file["W'=W - W avg"] = file['W']-average_w
 # -ve , +ve 2nd quadrant
 # -ve, -ve 3rd quadrant
 # +ve, -ve 4th quadrant
-octant_list =[]
+octant_list = []
 for x in range(len(file)):
-    
+
     if (file["U'=U - U avg"][x] > 0 and file["V'=V - V avg"][x] > 0):
         if (file["W'=W - W avg"][x] >= 0):
             octant_list.append(1)
         else:
             octant_list.append(-1)
 
-    elif (file["U'=U - U avg"][x] < 0 and file["V'=V - V avg"][x]> 0):
+    elif (file["U'=U - U avg"][x] < 0 and file["V'=V - V avg"][x] > 0):
         if (file["W'=W - W avg"][x] >= 0):
             octant_list.append(2)
         else:
@@ -48,9 +51,35 @@ for x in range(len(file)):
         else:
             octant_list.append(-4)
 
-file["octant"]= octant_list
-file.at[2, ' ']="User Input"
-file.at[0, 'Octant ID'] =' '
+file["octant"] = octant_list
+
+file.at[2, ' '] = "User Input"
+# file.at[]
+
+list_main = []
+list_main.append('Overall Count')
+mod = 5000
+total_range = math.ceil(len(file)/mod)
+list_main.append("Mod {}".format(mod))
+
+start = 0
+
+for i in range(total_range):
+    if i == (total_range-1):
+        value = "{}-{}".format(start, len(file))
+        list_main.append(value)
+        continue
+
+    value = "{}-{}".format(start, start+mod-1)
+    list_main.append(value)
+
+    start = start+mod
+
+# print(list_main)
+file["Octant ID"] = list_main
+
+
+# counting total count of unique numbers
 file.at[0, '1'] = file["octant"].value_counts()[1]
 file.at[0, '-1'] = file["octant"].value_counts()[-1]
 file.at[0, '2'] = file["octant"].value_counts()[2]
